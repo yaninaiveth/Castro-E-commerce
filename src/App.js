37762d151e3c4962d9a85b1ac9b037d9c9c79f44
Yani <yaniveth13@gmail.com>
2021-10-19 18:42:19 -0300

    Desafío 4: Contador con Botón

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
+import ItemCount from './components/ItemCount'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar/>
-        <ItemListContainer items={[1,2,3,4,5,6,7,8,9,10]} greeting="Bienvenido a mi Tienda"/>
+        <ItemListContainer items = { [1,2,3,4,5,6,7,8,9,10] } greeting = "Bienvenido a mi Tienda"/>
+        <ItemCount initial = "0" onAdd = {() => alert("Alert")} stock = "5"/>
       </Router>
     </div>
   );
---
+++ src/components/ItemCount.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+
+function ItemCount({ stock, initial, onAdd }){
+
+    const [count, setCount] = useState(Number(initial));
+
+    const add = () =>{
+        if (count > 0) onAdd();
+    }
+
+    const increment = (count) => {
+        if (count < stock) setCount(count + 1);
+    }
+
+    const decrement = (count) => {
+        if ((count > 0) || (count === 1)) setCount(count - 1);
+    }
+
+    return (
+        <div style = {{margin: "0 33% 0 33%", border: "solid 1px black", padding: "10px, 10px, 10px, 10px", background: "#E5EEFB"}}>
+            <h3>Cantidad de artículos: {count}</h3>
+
+            <div style = {{margin: "10px 0 0 0", padding: "10px 10px 10px 10px"}}>
+                <button style = {{margin: "5px 5px 5px 5px"}} onClick = {() => increment(count)}>Agregar</button>
+                <button style = {{margin: "5px 5px 5px 5px"}} onClick = {() => decrement(count)}>Quitar</button>
+            </div>
+            
+            <button style = {{margin: "8px 0 0 10"}} onClick = {add}>Añadir todo al carrito</button>
+        </div>
+    );
+}
+
+export default ItemCount;
