0f5245751f2bb313272d1cbafe466531ba485777
Yani <yaniveth13@gmail.com>
2021-11-02 21:08:08 -0300

    Desafío 9: Sincronizar Counter (arreglar)

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import ItemCount from './ItemCount';
+import { Link } from 'react-router-dom';
 
-const ItemDetail = ({title, description, pictureUrl}) => {
+const ItemDetail = ({title, description, pictureUrl, count, setCount}) => {
+
+    const [show, setShow] = useState(false)
+
+ 
+    const onAdd = () =>{
+        if (count > 0) {
+            setShow(true);
+
+            console.log("entra al if")
+        }
+    } 
+
 
     return (
 
         <div style = {{display:"inline-block"}}>
-            <img src={pictureUrl} className="card-img-top" alt="..."/>
+            <img src={pictureUrl} className="card-img-top" style = {{width: "280px"}} alt="..."/>
             <div className="card-body">
                 <h2>Item Detail</h2>
                 <h3 className="card-title">{title}</h3>
                 <p className="card-text">{description}</p>
-                <ItemCount initial = {0} onAdd = {() => alert("Alert")} stock = {5}/>
-            </div>
-        </div>
-    );
-};
 
+                {show === false && (<ItemCount stock = {5} initial = {0} count = {count} setCount = {setCount} />)} 
+                {show === false && count > 0 && (<button style = {{margin: "8px 0 0 10"}} onClick = {onAdd}>Añadir todo al carrito</button>)}
+
+                {show === true && count > 0 && (
+                <Link to="/cart"> 
+                    <button>Ir al carrito</button>
+                </Link>)}
+            
+            </div>
+
+                
+        </div>  
+                  
+)}
 export default ItemDetail
